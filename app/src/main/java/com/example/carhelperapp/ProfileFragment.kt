743b0f4c769a345b23d76bcefe34b6bc671f7b66
zDarKraDz <kostya.li.2015@mail.ru>
2025-05-20 20:27:19 +0300

    Исправление ошибок

--- app/src/main/java/com/example/carhelperapp/ProfileFragment.kt
+++ app/src/main/java/com/example/carhelperapp/ProfileFragment.kt
@@ -4,19 +4,25 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.example.carhelperapp.databinding.FragmentProfileBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.ktx.firestore
 
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private val auth = Firebase.auth
+    private val db = com.google.firebase.ktx.Firebase.firestore
+    private var isAdmin: Boolean = false
+    private val userId = Firebase.auth.currentUser?.uid ?: ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,8 +44,39 @@ class ProfileFragment : Fragment() {
             signOut()
             navigateToStartActivity()
         }
+
+        if (isAdmin) {
+            binding.mechanicView.visibility = View.VISIBLE
+        } else {
+            checkAdminStatus(userId = userId)
+        }
+
+
     }
 
+    private fun checkAdminStatus(userId: String) {
+        val adminRef = db.collection("adminAccount").document("accounts")
+        adminRef.get()
+            .addOnSuccessListener { document ->
+                if (!isAdded || _binding == null) return@addOnSuccessListener
+
+                // Получаем список id админов
+                val adminIds = document.get("id") as? List<String> ?: emptyList()
+
+                // Проверяем является ли текующий пользователь админом
+                if (adminIds.contains(userId)) {
+                    binding.mechanicView.visibility = View.VISIBLE
+                    Log.d("ProfileFragment", "User is admin")
+                    isAdmin = true
+                } else {
+                    binding.mechanicView.visibility = View.GONE
+                    Log.d("ProfileFragment", "User is NOT admin. Admin IDs: $adminIds, User ID: $userId")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("ProfileFragment", "get failed with ", exception)
+            }
+    }
     private fun exitApp() {
         requireActivity().finishAffinity()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
